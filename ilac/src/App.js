bce73a6c2ac64f6d6531bb651430133aff843686
kevser <kevsersemiz6161@gmail.com>
2025-08-02 12:10:10 +0300

    proje oluşturuldu

---
+++ ilac/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>🧠 İlaç Anlatan Kamera</h1>
+      <p>Proje başarıyla çalışıyor! Buradan devam edebilirsin 👇</p>
+    </div>
+  );
+}
+
+export default App;
